69ac0a9032990b520fc7293ab66ff77b9fc92690
eeshapenkey3-cpu <eeshapenkey3@gmail.com>
2026-04-11 11:48:02 +0100

    Add query for total debt of restricted customers
    Added a query to calculate total debt owed by restricted customers.

--- Eesha_Queries.sql
+++ Eesha_Queries.sql
@@ -2,6 +2,22 @@ USE library_CA1_GroupE;
 -- eesha queries
 /* query 1: dead debt
 identify how much money is owed by people who are already banned (restricted) */
+USE library_CA1_GroupE;
+
+SELECT 
+    C.fname,   
+    C.lname,   
+    SUM(F.fine_amount) AS total_debt_owed,
+    CASE 
+        WHEN SUM(F.fine_amount) > 15.00 THEN 'RESTRICTED'
+        ELSE 'Active'
+    END AS account_status
+FROM CUSTOMERS C
+JOIN LOANS L ON C.customer_id = L.customer_id
+JOIN FINES F ON DATEDIFF(L.date_returned, L.date_due) = F.days_overdue
+WHERE L.date_returned > L.date_due
+GROUP BY C.customer_id, C.fname, C.lname
+HAVING SUM(F.fine_amount) > 0.00;
 
 /* query 2: budget replacement forecast
 identify which devices are getting too old; flags old equipment */
